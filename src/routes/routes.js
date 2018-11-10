dff3da2e8365f3b120a9fd62263bf95d6ccd1153
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-11-10 15:42:46 -0200

    Continue remove unecessary code

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,25 +4,12 @@ import ContainerList from '@/components/InAttendance/Index'
 import AddPatient from '@/components/Patient/Index'
 import AddDoctor from '@/components/Doctor/Index'
 import Prescription from '@/components/InAttendance/Prescription'
-import UserProfile from '@/pages/UserProfile.vue'
-// import TableList from '@/pages/TableList.vue'
-// import Typography from '@/pages/Typography.vue'
-// import Icons from '@/pages/Icons.vue'
-// import Maps from '@/pages/Maps.vue'
-// import Notifications from '@/pages/Notifications.vue'
-// import UpgradeToPRO from '@/pages/UpgradeToPRO.vue'
-
 const routes = [
   {
     path: '/',
     component: DashboardLayout,
     redirect: '/in-attendance',
     children: [
-      {
-        path: 'user',
-        name: 'User Profile',
-        component: UserProfile
-      },
       {
         path: 'in-attendance',
         name: 'Em atendimento',
